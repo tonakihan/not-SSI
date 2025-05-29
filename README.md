4f2e644eab8a5f0d896f91ed566b510627dec24b
tonakihan <116086641+tonakihan@users.noreply.github.com>
2025-05-29 14:38:21 +0900

    Release v0.0.1

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # not-ssi
-Позволяет исопльзовать синтаксис SSI в html.  
-Создавался только для сбора одного html из нескольких.
+Allows you to using the syntax of SSI in html.
+It was created only to collect one HTML of several.
+
+## Interface
+So far, available next tags:
+- include `<!--#include file=""-->`
+
+## Launch
+Launch in cli
+```sh
+#Template: node [params_nodejs] index.js [params_of_script] path/to/your_target.html
+node index.js your_target.html
+
+# Or from another place in system
+#Template: node [params_nodejs] path/to/index.js [params_of_script] your_target.html
+```
+Input (target.html)
+```html
+<html>
+    <some_tags />
+    <!-- #include file="maps.html" -->
+</html
+```
--- index.js
+++ index.js
@@ -17,37 +17,45 @@ class SSI {
     dirname: "",
     output: ["build", "output.html"],
   };
-  readLine;
+  rl;
 
   constructor(file) {
     console.log(`INFO: Works dir name (PWD) = ${PWD}`);
+    console.log(`INFO: Out put = ${this.config.output.join(path.sep)}`);
 
     this.config.file = file;
     this.config.dirname = path.dirname(file);
 
-    this.readLine = readline.createInterface({
+    this.rl = readline.createInterface({
       input: fs.createReadStream(file),
-      output: fs.createWriteStream(this.config.output.join(path.sep)),
+      terminal: false,
     });
   }
 
   async processFile() {
-    fs.promises
+    await fs.promises
       .mkdir(this.config.output.slice(0, -1).join(path.sep), {
         recursive: true,
       })
       .then((_) => {
-        return fs.promises.appendFile(this.config.output.join(path.sep), "");
+        return fs.promises.appendFile(this.config.output.join(path.sep), "", {
+          flag: "w",
+          encoding: "utf-8",
+        });
       })
       .then(
-        this.readLine.on("line", (line) => {
-          console.debug("DBG: processFile: readline line:", line);
+        this.rl.on("line", (line) => {
+          //console.debug(`DBG: processFile: rl line: ${line} END_DBG_MSG`);
 
+          // TODO: Можно и вынести в _processLine
           if (this.regexp.test(line)) {
-            //this.readLine.write(this._processLine(line.trim()));
-          } else {
-            //this.readLine.write(line);
+            line = this._processLine(line);
           }
+
+          fs.promises.appendFile(
+            this.config.output.join(path.sep),
+            `${line}\n`,
+          );
         }),
       )
       .catch((e) => {
@@ -58,7 +66,7 @@ class SSI {
   /** If in the function has a match instuction, then returned a new string else returned the input line.
   @param line string. Example: '<!--#include file="none.html"-->'*/
   _processLine(line) {
-    switch (this.regexp.exec(line)) {
+    switch (this.regexp.exec(line)[1] || "") {
       case "include":
         return include.call(this, line);
         break;
@@ -68,14 +76,15 @@ class SSI {
     return line;
 
     /** Returnes a data of file specific in the line */
-    function include(indexLine, line) {
+    function include(line) {
+      //console.debug("DBG: include input:", line);
       const local_regexp = new RegExp(/#include file=["|'](.*)["|']/);
       // This path is relative of input file
       let filePath = local_regexp.exec(line)[1];
       // Change it
       filePath = `${this.config.dirname}${path.sep}${filePath}`;
 
-      const data = fs.readFileSync(filePath, { encoding: "utf8", flag: "r" });
+      const data = fs.readFileSync(filePath, { encoding: "utf-8", flag: "r" });
 
       //console.debug("DBG: data =", data);
 
